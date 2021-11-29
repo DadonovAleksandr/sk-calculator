a4469417bfe562edf0ebaf8db639f9f4b950d787
DadonovAD <DadonovAD@tvv.transneft.ru>
2021-11-29 16:34:47 +0300

    Initial commit

---
+++ CalculatorCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
